3c206e4c91406072abd87694b2c24ee218cb0333
Rahul Singh Aswal <91177958+RahulSinghAswal@users.noreply.github.com>
2022-09-21 16:07:55 +0530

    Update hello-world.c

--- Assignment/Assignment2/hello-world.c
+++ Assignment/Assignment2/hello-world.c
@@ -1,1 +1,18 @@
+// HackerRanker - https://www.hackerrank.com/challenges/hello-world-c/submissions/code/286487965
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[100];
+    scanf("%[^\n]%*s", &s);
+  	
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    printf("Hello, World! \n%s", s );   
+    return 0;
+}
 
