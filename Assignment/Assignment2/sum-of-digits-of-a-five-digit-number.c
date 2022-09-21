210c30226e95ac27b7205d538929fd2b1089630b
Rahul Singh Aswal <91177958+RahulSinghAswal@users.noreply.github.com>
2022-09-21 21:12:49 +0530

    Create sum-of-digits-of-a-five-digit-number.c

---
+++ Assignment/Assignment2/sum-of-digits-of-a-five-digit-number.c
@@ -0,0 +1,21 @@
+// HackerRanker - https://www.hackerrank.com/challenges/sum-of-digits-of-a-five-digit-number/submissions/code/286636334
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	
+    int n,rem,sum=0;
+    scanf("%d", &n);
+    
+    while (n>0){
+        rem = n%10;
+        sum = sum + rem;
+        n = n/10;
+        
+    }
+    printf("%d", sum);
+    return 0;
+}
