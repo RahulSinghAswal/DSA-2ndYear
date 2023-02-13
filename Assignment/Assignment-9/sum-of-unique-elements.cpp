f0c31197fe23b9137f6dc2ffcdea1d1c6519a5c9
Rahul Singh Aswal <91177958+RahulSinghAswal@users.noreply.github.com>
2023-02-13 23:05:56 +0530

    Create sum-of-unique-elements.cpp

---
+++ Assignment/Assignment-9/sum-of-unique-elements.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        int sum = 0;
+
+        unordered_map<int, int> mp;
+
+        for(int i = 0; i < nums.size(); i++ ) {
+            mp[nums[i]]++;
+        }
+        
+        for(auto i : mp) {
+            if (i.second < 2) {
+                sum = sum + i.first;
+            }
+        }
+
+
+        
+
+        return sum;
+        
+    }
+};
