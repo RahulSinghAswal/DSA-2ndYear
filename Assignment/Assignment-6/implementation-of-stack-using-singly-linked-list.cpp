a4e95f6919a0a05c96967f08b4c8edb709106cbe
Rahul Singh Aswal <91177958+RahulSinghAswal@users.noreply.github.com>
2023-02-14 16:39:06 +0530

    Update implementation-of-stack-using-singly-linked-list.cpp

--- Assignment/Assignment-6/implementation-of-stack-using-singly-linked-list.cpp
+++ Assignment/Assignment-6/implementation-of-stack-using-singly-linked-list.cpp
@@ -1,94 +1,80 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct Node{
+class Node {
+public:
 	int data;
-	Node *next;
+	Node* next;
+
+	Node(int n)
+	{
+		this->data = n;
+		this->next = nullptr;
+	}
 };
 
-Node* createNode(int val){
-	Node *temp = new Node;
-	temp->data = val;
-	return temp;
-}
+class Stack {
+	Node* top;
 
-//complete the functions
+public:
+	Stack(){
+    top = NULL;
+  }
 
-class Queue{
-	Node *head;
-	Node *tail;
-	int s;
-	
-	public:
-	//constructor is called whenever an object of the class is declared
-	Queue(){
-		cout<<"constructor is Called\n";
-		s=0;
-		head=NULL;
-		tail=NULL;
-	}
-	
-	//push the value at the back
-	void push(int val){
-		cout<<"pushing the val: "<<val<<" at the back of queue\n";
-		Node* temp=new Node();
-		temp->data=val;
-		temp->next=NULL;
-		if(head==NULL){
-			head=temp;
-			tail=temp;
+	void push(int data)
+	{
+
+		Node* temp = new Node(data);
+		if (!temp) {
+			cout << "\nStack Overflow";
 		}
-		else{
-			tail->next=temp;
-			tail=temp;
-		}
+		temp->data = data;
+		temp->next= top;
+		top = temp;
 	}
-		
-	int front(){
-		cout<<"returning the value at front\n";
-		return head->data;
-	}
-	
-	void pop(){
-		cout<<"removing the first element\n";
-		head=head->next;
-	}
-	
-	int size(){
-		int count=0;
-		Node* ptr=head;
-		cout<<"return the size of the queue\n";
-		while(ptr!=NULL){
-			count=count+1;
-			ptr=ptr->next;
-		}
-		return count;
-	}
-	
+
 	bool isEmpty(){
-		cout<<"returning if the queue is empty\n";
-		return (head==NULL);
+		return top == NULL;
 	}
+
+	int peek(){
+		if (!isEmpty())
+			return top->data;
+		else
+			return -1;
+	}
+
+
+	void pop()
+	{
+		Node* temp;
+		if (top == NULL) {
+			cout << "\nStack Underflow" << endl;
+			exit(1);
+		}
+
+		else {
+			temp = top;
+			top = top->next;
+		}
+	}
+
+
 };
 
-int main() {
-	Queue q; //constructor is called on this line
-	q.push(4);
-	cout<<"front: "<<q.front()<<"\n";
-	q.pop();
-	cout<<"size: "<<q.size()<<"\n";
-	cout<<"empty: "<<q.isEmpty()<<"\n";
-	
-	q.push(5);
-	q.push(3);
-	cout<<"front: "<<q.front()<<"\n";
-	q.pop();
-	cout<<"size: "<<q.size()<<"\n";
-	cout<<"empty: "<<q.isEmpty()<<"\n";
-	
-	cout<<"front: "<<q.front()<<"\n";
-	q.pop();
-	cout<<"size: "<<q.size()<<"\n";
-	cout<<"empty: "<<q.isEmpty()<<"\n";
+int main()
+{
+	Stack st;
+	st.push(1);
+	st.push(2);
+	st.push(3);
+	st.push(4);
+
+	cout<< st.peek() << endl;
+
+	st.pop();
+	st.pop();
+	cout <<st.peek() << endl;
+
 	return 0;
 }
